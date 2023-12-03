72be41a0a7efbf342834890cfafa7ce517df338c
jchodon <jigcho27@gmail.com>
2023-12-02 20:27:52 -0500

    project directory created

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+// eslint-disable-next-line no-unused-vars
+import React from "react";
+// @ts-ignore
+import styles from "./App.module.css";
+function App() {
+  return <div className={styles.App}>Hello World!</div>;
+}
+
+export default App;
