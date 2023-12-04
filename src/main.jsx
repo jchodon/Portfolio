2c385acb871bc66394f1958648ad09566f98e31a
jchodon <jigcho27@gmail.com>
2023-12-04 10:54:38 -0500

    util file added

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import App from "./App.jsx";
 import "./index.css";
 import "@fontsource/montserrat";
 import "@fontsource/outfit";
+// @ts-ignore
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
