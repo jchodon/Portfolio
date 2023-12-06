08dda8cef61186d0aab512ebe3379e87b7408665
jchodon <jigcho27@gmail.com>
2023-12-06 16:06:45 -0500

    hero section styling

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,20 +1,27 @@
 import React from "react";
 import { getImageUrl } from "../../utils";
+// @ts-ignore
 import styles from "./Hero.module.css";
 const Hero = () => {
   return (
-    <section>
-      <div>
-        <h1>Hi, I am Jigme</h1>
-        <p>
+    <section className={styles.container}>
+      <div className={styles.content}>
+        <h1 className={styles.title}> Hello!</h1>
+        <p className={styles.description}>
           I am a full-stack developer currently based in Toronto. I am
           passionate about creating inclusive and interactive user experience.
         </p>
-        <a href="mailto:jigcho27@gmail.com">Contact Me</a>
+        <a href="mailto:jigcho27@gmail.com" className={styles.contactBtn}>
+          Contact Me
+        </a>
       </div>
-      <img src={getImageUrl("hero/HeroImage.png")} alt="Hero Image of  me" />
+      <img
+        src={getImageUrl("hero/HeroImage.png")}
+        alt="Hero Image of  me"
+        className={styles.heroImg}
+      />
       <div className={styles.topBlur} />
-      <div className={styles.bottonbBlur} />
+      <div className={styles.bottomBlur} />
     </section>
   );
 };
