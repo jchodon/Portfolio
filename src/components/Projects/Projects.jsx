851fc728c3ff26ac0e43b181524ed0ab2641f56a
jchodon <jigcho27@gmail.com>
2023-12-08 16:15:16 -0500

    new color scheme for site

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -7,9 +7,9 @@ import ProjectCard from "./ProjectCards";
 import styles from "./Projects.module.css";
 function Projects() {
   return (
-    <section className={styles.container}>
+    <section className={styles.container} id="projects">
       <h2 className={styles.title}>Projects</h2>
-      <div className={styles.project}>
+      <div className={styles.projects}>
         {projects.map((project, id) => {
           return <ProjectCard key={id} project={project} />;
         })}
