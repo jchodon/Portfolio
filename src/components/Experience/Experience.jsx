61b1a270a506a1002cb091a77f3ce4b1d28d5673
jchodon <jigcho27@gmail.com>
2023-12-11 19:19:03 -0500

    experience jsx file

--- src/components/Experience/Experience.jsx
+++ src/components/Experience/Experience.jsx
@@ -2,16 +2,17 @@ import React from "react";
 import skills from "../data/skills.json";
 import history from "../data/history.json";
 import { getImageUrl } from "../../utils";
+import styles from "./Experience.module.css";
 function Experience() {
   return (
-    <section id="experience">
-      <h2>Experience</h2>
-      <div>
-        <div>
+    <section className={styles.container} id="experience">
+      <h2 className={styles.title}>Experience</h2>
+      <div className={styles.content}>
+        <div className={styles.skills}>
           {skills.map((skill, id) => {
             return (
-              <div key={id}>
-                <div>
+              <div className={styles.skill} key={id}>
+                <div className={styles.skillImageContainer}>
                   <img src={getImageUrl(skill.imageSrc)} alt={`skill.title`} />
                 </div>
                 <p>{skill.title}</p>
@@ -19,15 +20,15 @@ function Experience() {
             );
           })}
         </div>
-        <ul>
+        <ul className={styles.history}>
           {history.map((historyItem, id) => {
             return (
-              <li key={id}>
+              <li key={id} className={styles.historyItem}>
                 <img
                   src={getImageUrl(historyItem.imageSrc)}
                   alt={`${historyItem.organisation} Logo`}
                 />
-                <div>
+                <div className={styles.historyItemDetails}>
                   <h3>{`${historyItem.role} ${historyItem.organisation}`}</h3>
                   <h3>{`${historyItem.startDate}`}</h3>
                   <ul>
