990c5cab46a92324dbeaf9aecc18506331212c0f
jchodon <jigcho27@gmail.com>
2023-12-13 10:22:37 -0500

    contact section

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -7,7 +7,7 @@ function Contact() {
     <footer id="contact" className={styles.container}>
       <div className={styles.text}>
         <h2>Contact</h2>
-        <p> Feel free to reach out </p>
+        <p> Feel free to reach out! </p>
       </div>
       <ul className={styles.links}>
         <li className={styles.link}>
