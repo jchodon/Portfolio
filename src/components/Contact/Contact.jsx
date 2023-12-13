b2ce6dc6dbb577e7cf5e86e52c75b2d5e70162df
jchodon <jigcho27@gmail.com>
2023-12-12 19:01:31 -0500

    contact info added

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,18 +1,20 @@
 import React from "react";
+// @ts-ignore
+import styles from "./Contact.module.css";
 import { getImageUrl } from "../../utils";
 function Contact() {
   return (
-    <footer>
-      <div>
+    <footer id="contact" className={styles.container}>
+      <div className={styles.text}>
         <h2>Contact</h2>
         <p> Feel free to reach out </p>
       </div>
-      <ul>
-        <li>
+      <ul className={styles.links}>
+        <li className={styles.link}>
           <img src={getImageUrl("contact/emailIcon.png")} alt="Email Icon" />
           <a href="mailto:jigcho27@email.com">jigcho27@gmail.com</a>
         </li>
-        <li>
+        <li className={styles.link}>
           <img
             src={getImageUrl("contact/linkedinIcon.png")}
             alt="linkedin Icon"
@@ -21,7 +23,7 @@ function Contact() {
             linkedin.com/in/jigmec
           </a>
         </li>
-        <li>
+        <li className={styles.link}>
           <img src={getImageUrl("contact/githubIcon.png")} alt="Github Icon" />
           <a href="https://github.com/jchodon">github.com/jchodon</a>
         </li>
